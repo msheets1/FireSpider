a36aaead6e832d86b1f8971ed875981232336a0c
msheets1 <matthewsheets1@live.com>
2014-03-03 17:24:31 -0700

    Basic Textbook mockup
    Just finished off a basic mockup of some of the desired features. I
    spent too long, but I did have a fun time. ;) Just load up the
    'viewer.html' in your browser once you've cloned this.

---
+++ globalBook/exampleText1.js
@@ -0,0 +1,37 @@
+window.exampleText1 = {
+	meta: {},
+	body: {
+		sections: [
+			{
+				head: {
+					name: "First Things",
+					number: "1",
+				},
+				body: {
+					chapters: [
+						{
+							head: {
+								name: "of first",
+								number: "1"
+							},
+							body: {
+								section: {
+									head: {
+										title: "These things are amazing"
+									},
+									body: {
+										paragraphs: [
+											{
+												text: "This is crazy..."
+											}
+										]
+									}
+								}
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+}
